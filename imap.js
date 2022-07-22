3e6e2dbde257e9c6b60536938ae622d96e772a29
stevesajeev1 <76565057+stevesajeev1@users.noreply.github.com>
2022-07-22 15:34:30 -0400

    Changed the IMAP Package and updated to use OAuth2

--- imap.js
+++ imap.js
@@ -1,4 +1,6 @@
-const notifier = require("mail-notifier");
+const Imap = require('node-imap');
+const xoauth2 = require('xoauth2');
+const util = require('util');
 const fs = require('fs');
 const dayjs = require('dayjs');
 const customParseFormat = require('dayjs/plugin/customParseFormat')
@@ -13,34 +15,14 @@ dayjs.extend(customParseFormat);
 let botClient;
 let debugging;
 
-const imap = {
-  username: config.imapEmail,
-  password: config.imapPassword,
-  host: "imap.gmail.com",
-  port: 993, // imap port
-  tls: true,
-  connTimeout: 10000, // Default by node-imap
-  authTimeout: 5000, // Default by node-imap,
-  debug: null, // Or your custom function with only one incoming argument. Default: null
-  tlsOptions: { rejectUnauthorized: false },
-  mailbox: "INBOX", // mailbox to monitor
-  searchFilter: ["UNSEEN", "FLAGGED"], // the search filter being used after an IDLE notification has been retrieved
-  markSeen: true, // all fetched email will be marked as seen and not fetched next time
-  fetchUnreadOnStart: true, // use it only if you want to get all unread email on lib start. Default is `false`,
-  mailParserOptions: {streamAttachments: true}, // options to be passed to mailParser lib.
-  attachments: false, // download attachments as they are encountered to the project directory
-  attachmentOptions: { directory: "attachments/" }, // specify a download directory for attachments
-}
-
 function mail(mail) {
   // mail processing code goes here
-  if (mail.from[0].address !== 'notifications@instructure.com') return;
+  if (mail.address !== 'notifications@instructure.com') return;
   // parse role
-  let channelID = parse(mail.from[0].name)[0];
-  let roleID = parse(mail.from[0].name)[1];
+  let channelID = parse(mail.name)[0];
+  let roleID = parse(mail.name)[1];
 
   const subject = mail.subject;
-  console.log(subject);
   const text = mail.text;
   if (debugging) {
     index.debug(text);
@@ -97,6 +79,148 @@ function mail(mail) {
   }
 };
 
+exports.start = (client, debug) => {
+    botClient = client;
+    debugging = debug;
+
+    let tokenGen = xoauth2.createXOAuth2Generator({
+      user: config.imapEmail,
+      clientId: config.clientId,
+      clientSecret: config.clientSecret,
+      refreshToken: config.refreshToken
+    });
+    
+    tokenGen.getToken((err, token) => {
+        if (err) {
+            return console.log(err);
+        }
+    
+        let imap = new Imap({
+            xoauth2: token,
+            host: 'imap.gmail.com',
+            port: 993,
+            tls: true,
+            tlsOptions: {
+                rejectUnauthorized: false
+            },
+            keepalive: {
+                interval: 10 * 1000,
+                idleInterval: 60 * 1000,
+                forceNoop: true
+            }
+        });
+        
+        imap.on('ready', () => {
+            console.log('imap is ready');
+            openInbox(imap);
+        });
+    
+        imap.on('error', (err) => {
+            console.log('IMAP error: ' + err);
+            console.log('rebooting imap');
+            imap.end();
+            imap.connect();
+        });
+    
+        imap.on('mail', () => {
+            imap.search([ ['OR', 'UNSEEN', 'NEW'], ['FROM', 'notifications@instructure.com'] ], (err, results) => {
+                if (!results || !results.length) {
+                    return;
+                }
+                if (err) {
+                    console.log('Error searching for new message: ' + err);
+                }
+                imap.setFlags(results, ['\\Seen'], (err) => {
+                    if (err) {
+                        console.log('Error marking messages as seen: ' + err);
+                    }
+                });
+                const f = imap.fetch(results, { 
+                    bodies: ['HEADER.FIELDS (FROM SUBJECT)', 'TEXT'],
+                    markSeen: true
+                });
+                f.on('message', (msg) => {
+                    let mail = {};
+                    msg.on('body', async (stream, info) => {
+                        if (info.which === 'TEXT') {
+                            let rawText = await streamToString(stream);
+                            let plainText = rawText.substring(rawText.indexOf("UTF-8") + 7, rawText.indexOf("text/html")).trim();
+                            let parsedText = plainText.substring(0, plainText.lastIndexOf("\n")).trim();
+                            parsedText = parsedText.substring(0, parsedText.lastIndexOf("\n")).trim();
+                            mail.text = parsedText;
+                        } else {
+                            let headers = await streamToString(stream);
+                            let from = Imap.parseHeader(headers)['from'][0];
+                            mail.name = from.substring(0, from.lastIndexOf(" "));
+                            mail.address = from.substring(from.indexOf("<") + 1, from.indexOf(">"));
+                            mail.subject = Imap.parseHeader(headers)['subject'][0];
+                        }
+                    });
+                    msg.once('end', () => {
+                      mail(mail);
+                    })
+                });
+                f.once('error', (err) => {
+                    console.log('Fetch error: ' + err);
+                });
+            });
+        })
+    
+        imap.connect();
+    });
+}
+
+function openInbox(imap) {
+  imap.openBox('INBOX', false, (err, box) => {
+      if (err) {
+          throw err;
+      }
+      // check if there were any emails before startup
+      imap.search([ 'UNSEEN', ['FROM', 'notifications@instructure.com'] ], (err, results) => {
+          if (!results || !results.length) {
+              return;
+          }
+          if (err) {
+              console.log('Error searching for unseen messages: ' + err);
+          }
+          imap.setFlags(results, ['\\Seen'], (err) => {
+              if (err) {
+                  console.log('Error marking messages as seen: ' + err);
+              }
+          });
+          const f = imap.fetch(results, { 
+              bodies: ['HEADER.FIELDS (FROM SUBJECT)', 'TEXT'],
+              markSeen: true
+          });
+          f.on('message', (msg) => {
+            let mail = {};
+            msg.on('body', async (stream, info) => {
+                if (info.which === 'TEXT') {
+                    let rawText = await streamToString(stream);
+                    let plainText = rawText.substring(rawText.indexOf("UTF-8") + 7, rawText.indexOf("text/html")).trim();
+                    let parsedText = plainText.substring(0, plainText.lastIndexOf("\n")).trim();
+                    parsedText = parsedText.substring(0, parsedText.lastIndexOf("\n")).trim();
+                    mail.text = parsedText;
+                } else {
+                    let headers = await streamToString(stream);
+                    let from = Imap.parseHeader(headers)['from'][0];
+                    mail.name = from.substring(0, from.lastIndexOf(" "));
+                    mail.address = from.substring(from.indexOf("<") + 1, from.indexOf(">"));
+                    mail.subject = Imap.parseHeader(headers)['subject'][0];
+                }
+              });
+              msg.once('end', () => {
+                mail(mail);
+              })
+          });
+          f.once('error', (err) => {
+              console.log('Fetch error: ' + err);
+          });
+      });
+  });
+}
+
+
 function parse(text) {
   let map = JSON.parse(fs.readFileSync('imapInfo.json')).classes;
   for (var key of map) {
@@ -106,18 +230,11 @@ function parse(text) {
   }
 }
 
-exports.start = (client, debug) => {
-    botClient = client;
-    debugging = debug;
-    const n = notifier(imap);
-    n.on('end', () => n.start()) // session closed
-      .on('mail', m => {
-        mail(m);
-      })
-      .on('error', () => n.start())
-      .start();
-    console.log('started imap listening');
-
-    // stop listening
-    // mailListener.stop();
+function streamToString (stream) {
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on('error', (err) => reject(err));
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+  });
 }
